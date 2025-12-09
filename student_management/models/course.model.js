b05af28aca49ae39f25f96a071373fd81324888f
sebarreau <sebarreau@solutions.ht>
2025-12-09 13:35:52 -0500

    suppression de fichiers inutiles

--- student_management/models/course.model.js
+++ student_management/models/course.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const CourseShema = mongoose.Schema({
-    name : {type: String, require: [true, "Please enter the course name"]},
-    code : {type: String, require: [true, "Please enter the code"]} 
+    name : {type: String, required: [true, "Please enter the course name"]},
+    code : {type: String, required: [true, "Please enter the code"]} 
 });
 
 const Course = mongoose.model('Course', CourseShema)
